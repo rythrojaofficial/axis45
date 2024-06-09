551002477c08ba92fe3bfb4a1729cc19592126cb
rythroja <jared@mercenarytricking.com>
2024-06-09 03:10:01 -0700

    add validation and submission

--- forms/rcg2024/rcg24.js
+++ forms/rcg2024/rcg24.js
@@ -1,9 +1,9 @@
-import { HtmlElement } from "../../scripts/htmlElement.js";
 import { populateForm } from "../../scripts/forms.js";
 
-
+let sections = []
+// form sections
 let contactPrompts = [
-    { legend: 'la lagende kick!' },
+    { legend: 'Contact Information' },
     {
         // name
         question: 'Full Name'
@@ -72,20 +72,248 @@ let contactPrompts = [
     }
 
 ]
+sections.push(contactPrompts)
+
+let drive1stWay = [
+    { legend: 'Transportation ' },
+    {
+        // SEA -> PDX
+        question: 'SEA -> PDX'
+        ,name: '' // if necessary
+        ,label: '' // if necessary label
+        ,placeholder: 'SEA -> PDX' // if necessary
+        ,description: '' // if necessary
+        ,type: 'radio' // text, name, email, number, checkbox, date, select, radio 
+        ,appendedOptions: [
+            'I have a car and want to drive'
+            ,"I have a car, and I'll drive if necessary"
+            ,"I don't have a car and I need a ride"
+            ,"I don't have a car but I don't need a ride"
+            
+        ] // if necessary from type 
+        ,required: true // true or false
+    },
+    {
+        // When do you plan on leaving?
+        question: 'When do you plan on leaving?'
+        ,name: '' // if necessary
+        ,label: '' // if necessary label
+        ,placeholder:'' // if necessary
+        ,description: '' // if necessary
+        ,type: 'radio' // text, name, email, number, checkbox, date, select, radio 
+        ,appendedOptions: [
+            'Friday Morning'
+            ,'Friday Noonish'
+            ,'Friday Evening'
+            ,'Saturday Morning'
+            ,'other'
+            
+        ] // if necessary from type 
+        ,required: true // true or false
+    },
+    {
+        // How many total seats do you have (including you)
+        question: 'How many total seats do you have (including you)'
+        ,name: '' // if necessary
+        ,label: '' // if necessary label
+        ,placeholder:'' // if necessary
+        ,description: '' // if necessary
+        ,type: 'radio' // text, name, email, number, checkbox, date, select, radio 
+        ,appendedOptions: [
+            '0 (passenger)'
+            ,'1 seat'
+            ,'2 seats'
+            ,'3 seats'
+            ,'4 seats'
+            ,'5 seats'
+            ,'6 seats'
+            
+        ] // if necessary from type 
+        ,required: true // true or false
+    },
+    {
+        // What neighborhood/area are you leaving from
+        question: 'What neighborhood/area are you leaving from'
+        ,name: '' // if necessary
+        ,label: 'What neighborhood/area are you leaving from' // if necessary label
+        ,placeholder:'' // if necessary
+        ,description: '' // if necessary
+        ,type: 'text' // text, name, email, number, checkbox, date, select, radio 
+        ,appendedOptions: [] // if necessary from type 
+        ,required: true // true or false
+    },
 
 
-
-let sections = [
-    contactPrompts
-    
 ]
+sections.push(drive1stWay)
+
+let drive2ndWay = [
+    { legend: 'Transportation ' },
+    {
+        // PDX -> SEA
+        question: 'PDX -> SEA'
+        ,name: '' // if necessary
+        ,label: '' // if necessary label
+        ,placeholder: 'PDX -> SEA' // if necessary
+        ,description: '' // if necessary
+        ,type: 'radio' // text, name, email, number, checkbox, date, select, radio 
+        ,appendedOptions: [
+            'I have a car and want to drive'
+            ,"I have a car, and I'll drive if necessary"
+            ,"I don't have a car and I need a ride"
+            ,"I don't have a car but I don't need a ride"
+            
+        ] // if necessary from type 
+        ,required: true // true or false
+    },
+    {
+        // When do you plan on leaving?
+        question: 'When do you plan on leaving?'
+        ,name: '' // if necessary
+        ,label: '' // if necessary label
+        ,placeholder:'' // if necessary
+        ,description: '' // if necessary
+        ,type: 'radio' // text, name, email, number, checkbox, date, select, radio 
+        ,appendedOptions: [
+            'Sunday Morning'
+            ,'Sunday Noonish'
+            ,'Sunday Night'
+            ,'other'
+            
+        ] // if necessary from type 
+        ,required: true // true or false
+    },
+    {
+        // How many total seats do you have (including you)
+        question: 'How many total seats do you have (including you)'
+        ,name: '' // if necessary
+        ,label: '' // if necessary label
+        ,placeholder:'' // if necessary
+        ,description: '' // if necessary
+        ,type: 'radio' // text, name, email, number, checkbox, date, select, radio 
+        ,appendedOptions: [
+            '0 (passenger)'
+            ,'1 seat'
+            ,'2 seats'
+            ,'3 seats'
+            ,'4 seats'
+            ,'5 seats'
+            ,'6 seats'
+            
+        ] // if necessary from type 
+        ,required: true // true or false
+    },
+    {
+        // What neighborhood/area are you returning to
+        question: 'What neighborhood/area are you returning to'
+        ,name: '' // if necessary
+        ,label: 'What neighborhood/area are you returning to' // if necessary label
+        ,placeholder:'' // if necessary
+        ,description: '' // if necessary
+        ,type: 'text' // text, name, email, number, checkbox, date, select, radio 
+        ,appendedOptions: [] // if necessary from type 
+        ,required: true // true or false
+    },
+
+
+]
+sections.push(drive2ndWay)
+let accomodations = [
+    { legend: 'Accomodations ' },
+    {
+        // Are you personally booking an Airbnb?
+        question: 'Which days are you personally booking an Airbnb?'
+        ,name: '' // if necessary
+        ,label: '' // if necessary label
+        ,placeholder: '' // if necessary
+        ,description: '' // if necessary
+        ,type: 'checkbox' // text, name, email, number, checkbox, date, select, radio 
+        ,appendedOptions: [
+            'Friday Night'
+            ,'Saturday Night'
+            ,'Sunday Night'
+        ] // if necessary from type 
+        ,required: false // true or false
+    },
+    {
+        // Where are you planning to sleep Friday night?
+        question: 'Where are you planning to sleep Friday night?'
+        ,name: '' // if necessary
+        ,label: '' // if necessary label
+        ,placeholder: '' // if necessary
+        ,description: '' // if necessary
+        ,type: 'radio' // text, name, email, number, checkbox, date, select, radio 
+        ,appendedOptions: [
+            'Gym-sleepover no matter what'
+            ,'Airbnb I personally booked'
+            ,'Airbnb someone else has booked (confirmed)'
+            ,"N/A: won't be there or have a private setup"
+        ] // if necessary from type 
+        ,required: true // true or false
+    },
+    {
+        // Where are you planning to sleep Saturday night?
+        question: 'Where are you planning to sleep Saturday night?'
+        ,name: '' // if necessary
+        ,label: '' // if necessary label
+        ,placeholder: '' // if necessary
+        ,description: '' // if necessary
+        ,type: 'radio' // text, name, email, number, checkbox, date, select, radio 
+        ,appendedOptions: [
+            'Gym-sleepover no matter what'
+            ,'Airbnb I personally booked'
+            ,'Airbnb someone else has booked (confirmed)'
+            ,"N/A: won't be there or have a private setup"
+        ] // if necessary from type 
+        ,required: true // true or false
+    },
+    {
+        // Where are you planning to sleep Sunday night?
+        question: 'Where are you planning to sleep Sunday night?'
+        ,name: '' // if necessary
+        ,label: '' // if necessary label
+        ,placeholder: '' // if necessary
+        ,description: '' // if necessary
+        ,type: 'radio' // text, name, email, number, checkbox, date, select, radio 
+        ,appendedOptions: [
+            'Gym-sleepover no matter what'
+            ,'Airbnb I personally booked'
+            ,'Airbnb someone else has booked (confirmed)'
+            ,"N/A: won't be there or have a private setup"
+        ] // if necessary from type 
+        ,required: true // true or false
+    },
+    {
+        // If you have your own Airbnb and want to share, how many spots do you have (including you)?
+        question: 'If you have your own Airbnb and want to share, how many spots do you have (including you)?'
+        ,name: '' // if necessary
+        ,label: 'If you have your own Airbnb and want to share, how many spots do you have (including you)?' // if necessary label
+        ,placeholder: '' // if necessary
+        ,description: '' // if necessary
+        ,type: 'select' // text, name, email, number, checkbox, date, select, radio 
+        ,appendedOptions: [
+            '0 spots: No Airbnb'
+            ,'1 spot total'
+            ,'2 spots total'
+            ,'3 spots total'
+            ,'4 spots total'
+            ,'5 spots total'
+            ,'6 spots total'
+        ] // if necessary from type 
+        ,required: false // true or false
+    },
+]
+sections.push(accomodations)
+
+//  form Information
+let title = 'RCG Field Trip Carpool Survey'
 let rcgForm = {
-    title: 'RCG Field Trip Carpool Survey'
-    ,id: ''
+    title: title
+    ,id: title.replace(/ /g,"-")
     ,method: 'POST'
     ,action: '#'
     ,sectionArray: sections
-    ,prompts: contactPrompts
-
+    ,submitMessage: 'Submitting. . . Please Wait 🙂'
+    ,submitError: '*Please check fields'
     }
 populateForm(rcgForm, document.querySelector('body'))
--- scripts/forms.js
+++ scripts/forms.js
@@ -31,7 +31,6 @@ let radioOptions = [
 
 let theForm = {
     title: ''
-    ,id: ''
     ,method: 'POST'
     ,action: '#'
     // ,sectionArray: sectionsArray
@@ -48,6 +47,7 @@ export function populateForm(formObject, targetElement){
         }, formObject.title)
     // populateSections(formObject.sectionArray, form.element)
     populateSections(formObject, form.element)
+    populateSubmit(formObject, form.element)
     
     
     
@@ -55,10 +55,27 @@ export function populateForm(formObject, targetElement){
     
 
 }
+function populateSubmit(formObject, parentElement){
+    let submitWrapper = new HtmlElement('div', parentElement)
+
+    let submitButton = new HtmlElement('button', submitWrapper.element,{}, 'Submit');
+    let submitMessageSpan = new HtmlElement('span', submitWrapper.element)
+        submitButton.element.addEventListener('click', ()=>{
+            switch (validateForm(formObject.id)){
+                case true:
+                    submitMessageSpan.element.textContent = formObject.submitMessage
+                    submitButton.element.style = 'display: hidden'
+                    break;
+                default:
+                    submitMessageSpan.element.textContent = formObject.submitError
+                    break;
+            }
+        })
+}
 function populateInputs(inputObject, parentElement){
     
     let theName = (inputObject.name !== '') ? inputObject.name : inputObject.question;
-    let input;
+    // let input;
     if (inputObject.label !== ''){
         let label = new HtmlElement('label', parentElement,{for: theName}, inputObject.label)
     }
@@ -74,9 +91,95 @@ function populateInputs(inputObject, parentElement){
                 )
             break;
         case 'select':
+            let newSelect = new HtmlElement('select', parentElement, 
+                {
+                    name: theName
+                    ,id: `select-${theName}`
+                    ,label: inputObject.label
+                    ,placeholder: inputObject.placeholder
+                    ,required: inputObject.required
+                }
+                )
+            // let newOption = new HtmlElement('option', newSelect.element, {}, 'blablah')
+                inputObject.appendedOptions.forEach((option)=>{
+                    let newOption = new HtmlElement('option',
+                    newSelect.element, {value: option}, option)
+                }
+                )
+            break;
+        case 'radio':
+            let newRadio = new HtmlElement('div', parentElement, 
+                {
+                    name: theName
+                    ,label: inputObject.label
+                    ,placeholder: inputObject.placeholder
+                }, theName
+                )
+            inputObject.appendedOptions.forEach((option)=>{
+            let newRadioDiv = new HtmlElement('div', newRadio.element)
+                switch(option){
+                    case 'other':
+                        let newOtherOption = new HtmlElement('input', newRadioDiv.element, 
+                            { 
+                                type: 'radio'
+                                ,name: theName
+                                ,value: 'other'
+                            })
+                        let newUserOption = new HtmlElement('input', newRadioDiv.element,
+                            {
+                                type: 'text'
+                                ,placeholder:'other'
+                            })
+                            newUserOption.element.addEventListener('change', ()=>{
+                                (newUserOption.element.value === '') ? 
+                                newOtherOption.element.value = 'other' :
+                                newOtherOption.element.value = newUserOption.element.value     
+                            })
+                        break;
+                    default:
+                        let newRadioOption = new HtmlElement('input', newRadioDiv.element, 
+                            { 
+                                type: 'radio'
+                                ,id: `option ${theName} ${option}`
+                                ,name: theName
+                                ,value: option 
+                            }, option)
+                            if(inputObject.required){newRadioOption.element.setAttribute('required', true)}
+                        let newRadioLabel = new HtmlElement('label', newRadioDiv.element,
+                            { for: newRadioOption.element.id}, option);
+                        break;
+                }
+                
+                
+                
+            })
+            break;
+        case 'checkbox':
+            let newCheckbox = new HtmlElement('div', parentElement, 
+                {
+                    name: theName
+                    ,label: inputObject.label
+                    ,placeholder: inputObject.placeholder
+                }, theName
+                )
+            inputObject.appendedOptions.forEach((option)=>{
+                let newCheckboxDiv = new HtmlElement('div', newCheckbox.element)
+                let newCheckboxOption = new HtmlElement('input', newCheckboxDiv.element, 
+                { 
+                    type: 'checkbox'
+                    ,id: `option ${theName} ${option}`
+                    ,name: theName
+                    ,value: option 
+                }, option)
+                if(inputObject.required){newCheckboxOption.element.setAttribute('required', true)}
+                let newCheckboxLabel = new HtmlElement('label', newCheckboxDiv.element,
+                { for: newCheckboxOption.element.id}, option);
+                })
+
+            
             break;
         default:
-            input = new HtmlElement('input', parentElement, 
+            let newInput = new HtmlElement('input', parentElement, 
                 {
                     name: (inputObject.name !== '') ? inputObject.name : inputObject.question
                     ,type: inputObject.type
@@ -99,4 +202,22 @@ function populateSections(formObject, parentElement){
                 populateInputs(obj, newSection.element)
             })
     })
+}
+function validateForm(formID){
+    let requiredArray = Array.from(document.getElementById(formID).querySelectorAll('[required = true]'))
+    let counter = 0;
+    requiredArray.forEach((element)=>{
+            if(element.checkValidity()){
+                counter++
+            }
+        })
+    switch (counter){
+        case requiredArray.length:
+            return true
+            break;
+        default:
+            return false
+            break;
+    }
+
 }
