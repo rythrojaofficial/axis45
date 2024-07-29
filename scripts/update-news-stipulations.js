b463a545914fb9d4fd03646766b0a076f3f0ecf6
rythrojaofficial <131824590+rythrojaofficial@users.noreply.github.com>
2024-07-29 07:15:04 -0700

    Update update-news-stipulations.js

--- scripts/update-news-stipulations.js
+++ scripts/update-news-stipulations.js
@@ -1,6 +1,6 @@
 // If today's session is canceled
 export let noSesh = {
-  cancelled: true,
+  cancelled: false,
   days: [],
   messages: [
     "normal Practices canceled due to Rose City Gathering",
