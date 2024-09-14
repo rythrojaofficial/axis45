d4604326161ef6d383ae76fc7c518ca5bad97c0b
rythroja <jared@mercenarytricking.com>
2024-09-13 18:37:05 -0700

    add community page

--- scripts/headscript.js
+++ scripts/headscript.js
@@ -75,6 +75,11 @@ const navContact = new NavText(
   "contact-link",
   "https://www.seattletricking.com/contact"
 );
+const navCommunity = new NavText(
+  "Community",
+  "community-link",
+  "https://www.seattletricking.com/discord"
+);
 //nav icon links
 const navInsta = new NavIcon(
   "logo-instagram",
