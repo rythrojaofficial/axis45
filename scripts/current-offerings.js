8ddf86b58f2d6ecaa9b5a1dd1013222a3a925bcf
rythrojaofficial <131824590+rythrojaofficial@users.noreply.github.com>
2025-09-17 19:49:31 -0700

    update list on current-offerings.html, update current-offerings.js library

--- scripts/current-offerings.js
+++ scripts/current-offerings.js
@@ -92,6 +92,13 @@ let offeringsLibrary = {
     expLibrary[2],
     "Sunday"
   ),
+  openFlipping: new Offering(
+    "open-flipping",
+    "Extra Open mat time for our Tuesday and Thursday classes!  Airtrack and mats will be out and available.  Feel free to ask for tips if you've already got stuff you're working on!",
+    "120m",
+    expLibrary[0],
+    "Sunday"
+  ),
   workshops: new Offering(
     "workshops",
     "Rotating menu of comprehensive topics.",
