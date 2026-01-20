c78fcd7ae59e75b9ae90c8b977020f9cf128d809
rythrojaofficial <131824590+rythrojaofficial@users.noreply.github.com>
2026-01-20 14:51:33 -0800

    add copy text functionality

--- events/260131/display.js
+++ events/260131/display.js
@@ -2,3 +2,14 @@ import { mdElement } from "../../scripts/htmlElement.js";
 
 let display = document.getElementById('md-display')
 let mdEl = new mdElement(display,{}, "./backflip-a-thon.md")
+
+let pageText = document.getElementById('share-this-page-link');
+let copyButton = document.getElementById('copy-text-button');
+copyButton.addEventListener('click', ()=>{
+    pageText.select();
+    pageText.setSelectionRange(0,99999); // for mobile
+    navigator.clipboard.writeText(copyText.value);
+
+    alert(`text copied`)
+})
+
